3f9084e0d786841262a4e1daf97413275a2369eb
azdoolittle <drewdoolittle@gmail.com>
2020-04-27 11:15:42 -0400

    method for even-odd occurrences

--- src/ArrayPrograms.java
+++ src/ArrayPrograms.java
@@ -23,12 +23,12 @@ public class ArrayPrograms {
                 case 2:
                     removeDuplicates();
                     break;
-//                case 3:
-//                    sumsOf13();
-//                    break;
-//                case 4:
-//                    evenOddOccurrences();
-//                    break;
+                case 3:
+                    sumsOf13();
+                    break;
+                case 4:
+                    evenOddOccurrences();
+                    break;
 //                case 5:
 //                    parseString();
 //                    break;
@@ -42,6 +42,7 @@ public class ArrayPrograms {
     //1. Reverse String
     public static void reverseString() {
         Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Reverse String.");
         System.out.println("Enter a phrase to reverse.");
         String str = input.nextLine();
         char[] chArr = str.toCharArray();
@@ -64,7 +65,7 @@ public class ArrayPrograms {
     // If there are any duplicate values in that array, remove them and print out the remaining values.
     public static void removeDuplicates() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Remove Duplicates. ");
+        System.out.println("Welcome to Remove Duplicates.");
         int num[] = new int[10];
         int numLen = 10;
 
@@ -92,10 +93,53 @@ public class ArrayPrograms {
     }
     //3. Given array 1: [1,7,6,5,9] and array 2: [2,7,6,3,4]
     // Write a program that will print out all pairs from arrays 1 and 2 that gives a sum of 13:
+    public static void sumsOf13() {
+        System.out.println("Welcome to Sums of 13");
+        // initialize arrays
+        int[] arr1 = {1,7,6,5,9};
+        int[] arr2 = {2,7,6,3,4};
+        int len = 5;
+        String pairs = "";
+        // loop over both to find sums of 13
+        // if true, add to string
+        for (int i = 0; i < len; i += 1) {
+            for (int j = 0; j < len; j += 1) {
+                if (arr1[i] + arr2[j] == 13) {
+                    pairs += arr1[i] + ", " + arr2[j] + "\n";
+                }
+            }
+        }
+        System.out.println("Pairs that sum to 13:\n" + pairs);
+    }
     //4. Prompt the user to input 10 values and store them into an array.
     // Output the total number of odd and even values in the array.
+    public static void evenOddOccurrences() {
+        System.out.println("Welcome to Even and Odd Occurrences.");
+        Scanner input = new Scanner(System.in);
+        int num[] = new int[10];
+        int oddCount = 0;
+        int evenCount = 0;
+
+        //assign input to array
+        for (int i = 0; i < 10; i += 1) {
+            System.out.printf("Enter an integer (%d/10)\n", i + 1);
+            num[i] = input.nextInt();
+            // check if even or odd
+            if (num[i] % 2 == 0) {
+                evenCount += 1;
+            } else {
+                oddCount += 1;
+            }
+        }
+
+        System.out.printf("Even occurrences: %d\n" +
+                "Odd occurrences: %d\n", evenCount, oddCount);
+    }
     //5. Prompt the user to input one line of a string.
     // Parse the string into a word, print out the word and length of the word.
     // Assume that the maximum size of a line is 132.
-
+    public static void parseString() {
+        System.out.println("Welcome to Parse String.");
+        Scanner input = new Scanner(System.in);
+    }
 }
